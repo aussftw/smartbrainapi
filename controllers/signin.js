13f21eb9df8cc15cd4eb405f5331e769a7da5bf4
aussftw <shagronaus@gmail.com>
2019-01-02 21:46:29 +0200

    fixing singin

--- controllers/signin.js
+++ controllers/signin.js
@@ -8,17 +8,17 @@ const handleSignin = (db, bcrypt) => (req, res) => {
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
-        return db.select('*').from('useres')
+        return db.select('*').from('users')
           .where('email', '=', email)
           .then(user => {
             res.json(user[0])
           })
           .catch(err => res.status(400).json('unable to get user'))
       } else {
-        res.status(400).json('credentials incorrect') 
+        res.status(400).json('wrong credentials')
       }
     })
-    .catch(err => res.status(400).json('wrong combination of email and password'))
+    .catch(err => res.status(400).json('wrong credentials'))
 }
 
 module.exports = {
